0ca82bd62cdabd7f9b80d246f26c81af6b12ef68
Fernando <al222311228@gmail.com>
2025-05-14 10:35:15 -0600

    Registro con estilos

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
     res.send('¡Hola, mundo desde Express!');
 });
