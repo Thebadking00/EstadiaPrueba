bcd719886e0611f5385fa45177be11e235f429fc
Fernando <al222311228@gmail.com>
2025-05-14 11:51:27 -0600

    json

--- index.js
+++ index.js
@@ -9,15 +9,23 @@ app.get('/', (req, res) => {
 });
 
 app.get('/Adios', (req, res) => {
-    res.send('Adios gente, mundo desde Express!');
+  //  res.send('Adios gente, mundo desde Express!');
+    res.json({
+        message: 'Adios gente, mundo desde Express!'});
 });
 
 app.post('/login' , (req, res) =>{
-    res.send('Bienvenido a la pagina de login');
+ //   res.send('Bienvenido a la pagina de login');
+     res.json({
+    message: 'LOgin correcto'
+ });
 });
 
 app.post('/registro', (req, res) => {
-    res.send('recibiendo datos');
+ //   res.send('recibiendo datos');
+ res.json({
+    message: 'recibiendo datos'
+ });
 });
 
 
